29cd16c4b60ed75967bb9e19824ee4ec08ffee89
Kelderth <Kelderth@gmail.com>
2018-12-14 14:07:51 -0500

    Making some progress with the Album Details View Controller.

---
+++ iTunes Top Album/Controllers/AlbumDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AlbumDetailsViewController.swift
+//  iTunes Top Album
+//
+//  Created by MCS on 12/14/18.
+//  Copyright © 2018 Jesus Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class AlbumDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
